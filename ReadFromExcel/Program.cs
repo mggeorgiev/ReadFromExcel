26b2e41fa8f0abd2cf6cb12bcc7889ae733643f2
Martin Georgiev <mggeorgiev@gmail.com>
2020-04-15 19:05:23 +0300

    Add project files.

---
+++ ReadFromExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadFromExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
